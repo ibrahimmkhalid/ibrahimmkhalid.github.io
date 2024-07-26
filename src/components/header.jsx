f93328e2700195351e890e769c218c7d682ace09
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2024-07-26 00:32:51 -0700

    update header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -40,71 +40,74 @@ function toggleMenu() {
 
 const Header = ({ isBlog = false }) => (
   <>
-    <header className="sticky top-0 flex h-14 items-center bg-white px-4 lg:px-6" id="#">
-      <span
-        className="items-center text-sm font-medium underline-offset-4 hover:underline"
-        onClick={'window.scrollTo({top:0, behavior: "smooth"})'}
-      >
-        {isBlog ? (
-          <>
-            <a href="/">
-              <i className="fas fa-arrow-left mr-1"></i>
+    <div className="sticky top-0">
+      <header className="flex h-14 items-center bg-white px-4 lg:px-6" id="#">
+        <span
+          className="items-center font-medium underline-offset-4 hover:underline"
+          onClick={'window.scrollTo({top:0, behavior: "smooth"})'}
+        >
+          {isBlog ? (
+            <>
+              <a href="/">
+                <i className="fas fa-arrow-left mr-1"></i>
+                Ibrahim Khalid
+              </a>
+            </>
+          ) : (
+            <>
+              <i className="fas fa-code mr-1"></i>
               Ibrahim Khalid
-            </a>
-          </>
+            </>
+          )}
+        </span>
+        {isBlog ? (
+          <></>
         ) : (
           <>
-            <i className="fas fa-code mr-1"></i>
-            Ibrahim Khalid
-          </>
-        )}
-      </span>
-      {isBlog ? (
-        <></>
-      ) : (
-        <>
-          <nav className="ml-auto hidden gap-4 sm:gap-6 md:flex">
-            {tabs.map((tab) => {
-              return (
-                <span
-                  key={tab.id}
-                  className="text-sm font-medium underline-offset-4 hover:underline"
-                  onClick={`document.getElementById("${tab.id}").scrollIntoView({behavior:"smooth"})`}
-                >
-                  {tab.name}
-                </span>
-              );
-            })}
-          </nav>
-          <div className="ml-auto gap-4 sm:gap-6 md:hidden">
-            <div className="flex hover:cursor-pointer" id="burger-menu-button" onclick="toggleMenu()">
-              <i className="fas fa-bars"></i>
-            </div>
-            <div
-              className="absolute left-0 top-0 z-30 hidden h-full w-full flex-col items-center justify-center bg-white"
-              style="height: 100vh; width: 100vw"
-              id="burger-menu"
-              onclick="toggleMenu()"
-            >
-              <div className="absolute right-0 top-0 mx-4 my-4 hover:cursor-pointer">
-                <i className="fas fa-times"></i>
-              </div>
+            <nav className="ml-auto hidden gap-4 sm:gap-6 md:flex">
               {tabs.map((tab) => {
                 return (
-                  <div
+                  <span
                     key={tab.id}
-                    className="my-4 rounded-md border-2 bg-gray-100 p-4 text-sm font-medium underline-offset-4 hover:cursor-pointer hover:underline"
+                    className="font-medium underline-offset-4 hover:underline"
                     onClick={`document.getElementById("${tab.id}").scrollIntoView({behavior:"smooth"})`}
                   >
                     {tab.name}
-                  </div>
+                  </span>
                 );
               })}
+            </nav>
+            <div className="ml-auto gap-4 sm:gap-6 md:hidden">
+              <div className="flex hover:cursor-pointer" id="burger-menu-button" onclick="toggleMenu()">
+                <i className="fas fa-bars"></i>
+              </div>
+              <div
+                className="absolute left-0 top-0 z-30 hidden h-full w-full flex-col items-center justify-center bg-white"
+                style="height: 100vh; width: 100vw"
+                id="burger-menu"
+                onclick="toggleMenu()"
+              >
+                <div className="absolute right-0 top-0 mx-4 my-4 hover:cursor-pointer">
+                  <i className="fas fa-times"></i>
+                </div>
+                {tabs.map((tab) => {
+                  return (
+                    <div
+                      key={tab.id}
+                      className="my-4 rounded-md border-2 bg-gray-100 p-4 font-medium underline-offset-4 hover:cursor-pointer hover:underline"
+                      onClick={`document.getElementById("${tab.id}").scrollIntoView({behavior:"smooth"})`}
+                    >
+                      {tab.name}
+                    </div>
+                  );
+                })}
+              </div>
             </div>
-          </div>
-        </>
-      )}
-    </header>
+          </>
+        )}
+      </header>
+      <hr />
+    </div>
   </>
 );
 
