3748ffd8ca8f4de8381778537d7e73c7eb5c0d69
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2024-08-08 20:06:13 -0700

    update project card to dispaly image if available

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -5,10 +5,12 @@ export const Project = ({ title, body, image, link = "", buttons = [] }) => {
     <>
       <div className="z-0 my-4 w-full rounded-lg border hover:shadow">
         <a className="h-auto pb-8" href={link} target="_blank">
-          <div className="relative overflow-hidden rounded-t-lg">
-            <img src={image} className="aspect-video object-cover transition-all" />
-            <div className="absolute inset-0 bg-gray-200/20"></div>
-          </div>
+          {image && (
+            <div className="relative overflow-hidden rounded-t-lg">
+              <img src={image} className="aspect-video object-cover transition-all" />
+              <div className="absolute inset-0 bg-gray-200/20"></div>
+            </div>
+          )}
           <div className="px-6 py-3">
             <h3 className="py-4 text-lg font-semibold tracking-tight">{title}</h3>
             <p className="pb-4 text-sm">{body}</p>
