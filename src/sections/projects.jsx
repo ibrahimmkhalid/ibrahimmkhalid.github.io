d9c5325ecfc0f83a32db1499bf7b5ffebf3621d7
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2024-08-08 20:02:14 -0700

    update project section

--- src/sections/projects.jsx
+++ src/sections/projects.jsx
@@ -2,13 +2,15 @@ import { projects } from "../data.js";
 import { Project } from "../components/project.jsx";
 
 export const Projects = () => {
+  const even = projects.filter((_, idx) => idx % 2 === 0);
+  const odd = projects.filter((_, idx) => idx % 2 !== 0);
   return (
     <>
       <h1 className="text-2xl font-bold">Projects</h1>
       <h2 className="pb-4 text-xl text-gray-600">Showcasing my skills</h2>
-      <div className="w-full grid-cols-2 gap-4 py-4 2xl:grid">
-        {projects.map((project) => {
-          return (
+      <div className="grid w-full grid-cols-1 gap-0 2xl:grid-cols-2 2xl:gap-4">
+        <div class="space-y-4">
+          {even.map((project) => (
             <Project
               title={project.title}
               body={project.body}
@@ -16,8 +18,19 @@ export const Projects = () => {
               image={project.image}
               link={project.link}
             />
-          );
-        })}
+          ))}
+        </div>
+        <div class="space-y-4">
+          {odd.map((project) => (
+            <Project
+              title={project.title}
+              body={project.body}
+              buttons={project.buttons}
+              image={project.image}
+              link={project.link}
+            />
+          ))}
+        </div>
       </div>
     </>
   );
