d9c1b9caf646d27d8ea7134af97d66a14c1dbb84
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2025-07-04 18:06:05 -0700

    fix site

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import "./tailwind.config.js"
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://ibrahimkhalid.me',
   integrations: [mdx(), sitemap(), react()],
 
   vite: {
